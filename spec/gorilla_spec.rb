b0243bb7ec79a046fb533505ab52797066643499
Ben Morimoto & suprfrye <pair+bkmorimoto.suprfrye@devbootcamp.com>
2015-04-21 17:23:03 -0400

    Add controller tests

--- spec/gorilla_spec.rb
+++ spec/gorilla_spec.rb
@@ -22,3 +22,29 @@ describe User do
 
   it { should have_many(:surveys_created) }
 end
+
+describe "Survey controller" do
+
+  it "should go to the new survey page" do
+    get '/surveys/new'
+    expect(last_response.body).to include('<input class="form-control new_input" type="text" name="survey[title]">')
+  end
+
+  it "should redirect to new signin if no current_user" do
+    get '/'
+    expect(last_response.redirect?).to be_true
+    follow_redirect!
+    expect(last_request.path).to eq('/signin')
+
+  end
+
+end
+
+
+# it "displays home page if there is no user" do
+#   get '/welcome', :user => nil
+
+#   expect(last_response.redirect?).to be_true
+#   follow_redirect!
+#   expect(last_request.path).to eq('/')
+# end
